e3def8ebb2647282168f69680fe3100498fecaeb
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-11-07 17:02:20 -0300

    Evitar alertas duplicadas

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function submitCita(e) {
 
     // some va a verificar que al menos un campo del objeto este vacio
     if (Object.values(citaObj).some(campo => campo.trim() === '')) {
-        const notificacion = new Notificacion({
+        new Notificacion({
             mensaje: 'Todos los campos son obligatorios',
             tipo: 'error'
         });
@@ -55,6 +55,13 @@ class Notificacion {
         const alerta = document.createElement('div');
         alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
 
+        // Eliminar alertas duplicadas
+        // const alertasExistentes = document.querySelectorAll('.alert');
+        // alertasExistentes.forEach(alerta => alerta.remove());
+        const alertaPrevia = document.querySelector('.alert');
+        // Optional chaining: Verifica si alertaPrevia existe antes de llamar al metodo remove
+        alertaPrevia?.remove();
+
         // Si es de tipo error, agrega una clase, si no, otra clase
         this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
 
@@ -63,5 +70,10 @@ class Notificacion {
 
         // insertBefore toma dos argumentos: 1. que elemento quiero insertar, 2. antes de que elemento lo quiero insertar
         formulario.parentElement.insertBefore(alerta, formulario);
+
+        // Quitar la alerta despues de 3 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
     }
 }
