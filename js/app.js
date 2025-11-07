180d547e7906469386d06632985f0fcbf0875a23
Kevin Acuña <94419050+kevinacuna1@users.noreply.github.com>
2025-11-06 23:41:32 -0300

    Creando el objeto de Cita para sincronizar

--- js/app.js
+++ js/app.js
@@ -4,3 +4,15 @@ const telefonoInput = document.querySelector('#telefono');
 const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
+
+// Objeto de Cita
+const obj = {
+    paciente: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+console.log(obj);
