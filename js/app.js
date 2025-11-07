68c2ef73f1f6033e142df22ec73f2e01b18d1e56
Kevin Acuña <94419050+kevinacuna1@users.noreply.github.com>
2025-11-06 23:04:23 -0300

    Comenzando con el proyecto

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const pacienteInput = document.querySelector('#paciente');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
